cf581145e2e001be2d15ca768448386ab44dd69c
Dmitry Tantsur <dtantsur@protonmail.com>
2020-01-07 10:53:23 +0100

    Try creating the whole DB schema before falling back to upgrades
    First, loop until 'dbsync version' can go through. Then try the
    simple `create_schema` command and fall back to `upgrade`.

--- runironic-conductor.sh
+++ runironic-conductor.sh
@@ -9,9 +9,18 @@ cp -f /tmp/uefi_esp.img /shared/html/uefi_esp.img
 
 # It's possible for the dbsync to fail if mariadb is not up yet, so
 # retry until success
-until ironic-dbsync --config-file /etc/ironic/ironic.conf upgrade; do
-  echo "WARNING: ironic-dbsync failed, retrying"
-  sleep 1
+until ironic-dbsync --config-file /etc/ironic/ironic.conf version; do
+    echo "WARNING: ironic-dbsync failed, retrying"
+    sleep 1
 done
+
+# Try creating the complete schema, it's much faster than upgrading.
+if ! ironic-dbsync --config-file /etc/ironic/ironic.conf create_schema; then
+    echo "Creating schema failed, falling back to schema upgrade"
+    ironic-dbsync --config-file /etc/ironic/ironic.conf upgrade || {
+        echo "ERROR: failed to create DB schema, cannot start the conductor";
+        exit 1;
+    }
+fi
 
 exec /usr/bin/ironic-conductor --config-file /etc/ironic/ironic.conf
